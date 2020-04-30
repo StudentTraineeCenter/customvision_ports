c516a4067cd92a2af93ff2b56d16284be0e6272a
Vasulda <vaclavkubon2@gmail.com>
2020-04-30 09:54:36 +0200

    changes

--- application.py
+++ application.py
@@ -1,23 +1,43 @@
+#backend aplikace
+#obsahuje infrastrukturu k přesměrování mezi stránkami, zpracovává snímky od uživatele
 from flask import Flask, render_template, request
 import os
 import base64
 import re
 from io import BytesIO
-import numpy as np
 from PIL import Image
 import sys
-sys.path.insert(1, 'python')
+sys.path.insert(1, 'python') #umožní importovat soubory z jiné složky
 from predict import main
-from helpfuncs import draw_boxes #extra functions used
 
+#funkce vykreslující boxy kolem detekovaných objektů
+def draw_boxes(image_data, predictions): 
+    #nejmenší validní počet nalezených objektů
+    if len(predictions) <= 1: 
+        return None
 
-app = Flask(__name__)
-app.config['IMAGE_UPLOADS'] = 'static/img/user_img'
-
-@app.route("/")
-def home():
+    image = Image.open(image_data)
+    img_width, img_height = image.size #předání rozměrů
+    img = ImageDraw.Draw(image) 
     
-    for file in os.listdir(app.config['IMAGE_UPLOADS']): #deleting images loaded from user before
+    for obj in predictions:
+        x0 = obj['boundingBox']['left'] * img_width  #horní levý roh
+        y0 = obj['boundingBox']['top'] * img_height
+        x1 = x0 + (obj['boundingBox']['width'] * img_width) #dolní pravý roh
+        y1 = y0 + (obj['boundingBox']['height'] * img_height)
+        shape = [(x0, y0), (x1, y1)]
+        img.rectangle(shape, outline ="red") #nakreslí box
+        img.text((x0 + 10, y0 + 10), obj['tagName'], fill="red") #přidá popisek
+
+    return image
+
+app = Flask(__name__) #vytvoření instance aplikace
+app.config['IMAGE_UPLOADS'] = 'static/img/user_img' #složka se snímky uživatelů
+
+@app.route("/") #základní menu
+def home():
+    #vymaže nahrané snímky od předchozích uživatelů
+    for file in os.listdir(app.config['IMAGE_UPLOADS']): 
 
         file_path = os.path.join(app.config['IMAGE_UPLOADS'], file)
 
@@ -33,30 +53,27 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/camera", methods=['GET', 'POST']) #main page
+@app.route("/camera", methods=['GET', 'POST']) #pořizování snímků
 def camera():
     return render_template('camera.html')
 
 
-@app.route("/detect", methods=['GET', 'POST']) #detection
+@app.route("/detect", methods=['GET', 'POST']) #zobrazení výsledků
 def detection():
     
     if request.method == 'POST':
 
-            image_b64 = request.values['image']
-            img_test = image_b64
-            image_b64 = re.sub('^data:image/.+;base64,', '', image_b64) #cutting the metadata
-            image_data = BytesIO(base64.b64decode(image_b64)) 
+            image_b64 = request.values['image'] #vyžádání obrázku z html form jako base64 data
+            image_b64 = re.sub('^data:image/.+;base64,', '', image_b64) #odříznutí metadat
+            image_data = BytesIO(base64.b64decode(image_b64)) #dekódovaní base64 dat
             
             image = Image.open(image_data)
-            image.show()
-            predictions = main(image_data)
-            image = draw_boxes(image_data, predictions)
-
-            if image == None: return render_template('choice.html')
-
-            image = image.convert("RGB")
-            image = image.save(os.path.join(app.config['IMAGE_UPLOADS'], '{}.jpg'.format(predictions[0]['probability'])))
+            predictions = main(image_data) #předání snímku customvision modelu
+            image = draw_boxes(image_data, predictions) #vykreslí boxy
+            #pokud nenajde dost nebo žádný objekt vrátí uživatele zpět
+            if image == None: return render_template('camera.html')
+            #uloží snímek s vykreslenými boxy
+            image = image.save(os.path.join(app.config['IMAGE_UPLOADS'], '{}.jpg'.format(predictions[0]['probability']))) 
 
             return render_template('choice.html', user_image=os.path.join(app.config['IMAGE_UPLOADS'], '{}.jpg'.format(predictions[0]['probability']))
             ,port1=predictions[0]['tagName'], port2=predictions[1]['tagName'])
